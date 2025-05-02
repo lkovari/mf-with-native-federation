cdca4cb340ab404a3261752f9ef7c88556ea2bce
lkovari <laszlo.kovary@gmail.com>
2025-05-02 20:10:54 +0200

    Updates

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-import{a as r}from"./chunk-OBBLAJGF.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-ZFT2GAPC.js")).catch(o=>console.error(o));
+import{a as r}from"./chunk-OBBLAJGF.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-3PKDPYJT.js")).catch(o=>console.error(o));
