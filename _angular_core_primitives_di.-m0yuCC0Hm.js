62891a4212c384a75178ee71adaac362754f4cc7
lkovari <laszlo.kovary@gmail.com>
2025-05-11 02:05:57 +0200

    Updates

---
+++ _angular_core_primitives_di.-m0yuCC0Hm.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e}from"./chunk-RUWNMG4G.js";import"./chunk-4CLCTAJ7.js";export{c as NOT_FOUND,d as NotFoundError,a as getCurrentInjector,e as isNotFound,b as setCurrentInjector};
--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-import{a as r}from"./chunk-OBBLAJGF.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-5WKEP743.js")).catch(o=>console.error(o));
+import{a as r}from"./chunk-GN2DVZQ6.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-V5TVOTVJ.js")).catch(o=>console.error(o));
